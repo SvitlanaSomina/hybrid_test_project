30b4c558b01f90cb29639ecd3373ffc0eb23a2e9
Svitlana Somina <Svitlana_Somina@epam.com>
2022-06-07 20:50:26 +0300

    Task HW_1 initial commit

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,28 @@
+package pages;
+
+import dataProvider.ConfigFileReader;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+
+public class BasePage {
+    WebDriver driver;
+
+    ConfigFileReader configFileReader = new ConfigFileReader();
+    private final long TIME_TO_WAIT = configFileReader.getDefaultWaitTime();
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void waitVisibilityOfElement(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(TIME_TO_WAIT));
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+}
---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,48 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+
+    @FindBy(xpath = "//input[@id='auth']")
+    private WebElement emailInput;
+
+    @FindBy(xpath = "//input[@id='v-login-password']")
+    private WebElement passwordInput;
+
+    @FindBy(xpath = "//button[@class='modal-submit-button']")
+    private WebElement loginButton;
+
+    @FindBy(xpath = "//span[contains(text(),'Невірна адреса електронної пошти (email) або пароль.')]")
+    private WebElement warningMessage;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public WebElement getEmailInput() {
+        return emailInput;
+    }
+
+    public void inputEmail(final String email) {
+        emailInput.sendKeys(email);
+    }
+
+    public void inputPassword(final String password) {
+        passwordInput.sendKeys(password);
+    }
+
+    public void clickOnLoginButton() {
+        loginButton.click();
+    }
+
+    public WebElement getWarningMessage() {
+        return warningMessage;
+    }
+
+    public String getWarningMessageText() {
+        return warningMessage.getText();
+    }
+}
