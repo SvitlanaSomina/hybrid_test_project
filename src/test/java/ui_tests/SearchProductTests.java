daf485b9bbaad68132a73c16982b23fe002d6242
Svitlana Somina <Svitlana_Somina@epam.com>
2022-07-23 22:44:00 +0300

    final changes in tests

--- src/test/java/ui_tests/SearchProductTests.java
+++ src/test/java/ui_tests/SearchProductTests.java
@@ -18,7 +18,6 @@ public class SearchProductTests extends BaseUiTest {
                 .clickSearchButton();
         getProductListPage().clickProductCardTitle();
         Assert.assertTrue(getProductPage().isPageUrlContainsText(QUERY_TEXT));
-        Assert.assertTrue(false);
     }
 
     @Test
@@ -35,6 +34,5 @@ public class SearchProductTests extends BaseUiTest {
         getCatalogPage().moveToSmartphonesAndPhonesButton();
         getCatalogPage().clickXiaomiButton();
         Assert.assertTrue(getProductListPage().isSearchResultsContainsSearchWord());
-        throw new SkipException("This test is skipped");
     }
 }
