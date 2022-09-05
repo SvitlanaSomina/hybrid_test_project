22c819740ababa281a71adbe027ce52e58c17de0
Svitlana Somina <Svitlana_Somina@epam.com>
2022-09-05 10:58:08 +0300

    code enhancement was made

---
+++ src/main/java/constants/RequestConstants.java
@@ -0,0 +1,14 @@
+package constants;
+
+public class RequestConstants {
+    public static final String URL = "https://reqres.in/";
+    public static final String EMAIL_SUCCESSFUL_LOGIN = "eve.holt@reqres.in";
+    public static final String EMAIL_UNSUCCESSFUL_LOGIN = "peter@klaven";
+    public static final String EMAIL_SUCCESSFUL_REGISTRATION = "eve.holt@reqres.in";
+    public static final String PASSWORD_SUCCESSFUL_LOGIN = "cityslicka";
+    public static final String PASSWORD_SUCCESSFUL_REGISTRATION = "pistol";
+    public static final String EMAIL_UNSUCCESSFUL_REGISTRATION = "sydney@fife";
+    public static final String WRONG_PASSWORD = "";
+    public static final String LOGIN_PATH = "api/login";
+    public static final String REGISTER_PATH = "api/register";
+}
